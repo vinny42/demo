0ee5217205699e40b829006d361d926222826da0
Vincent Elschot <vincent.elschot@genj.nl>
2017-10-20 11:57:10 +0200

    WIP

---
+++ src/bar/Tests/barTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+namespace bar;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class fooTest
+ */
+class BarTest extends WebTestCase
+{
+    /**
+     *
+     */
+    public function testFoo()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ src/foo/Tests/fooTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ */
+
+namespace foo;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class FooTest
+ *
+ */
+class FooTest extends WebTestCase
+{
+    /**
+     *
+     */
+    public function testFoo()
+    {
+        $this->assertTrue(true, "True should be True");
+    }
+}
